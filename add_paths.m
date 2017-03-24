2183aa8a463f7ef40831c57e75cd5efc6610e3d6
cjakobson <cjakobson@DN0a264121.SUNet>
2017-03-24 08:48:34 -0700

    Public code deposit
    Code to accompany PLoS Comp Bio manuscript

---
+++ add_paths.m
@@ -0,0 +1,14 @@
+% add paths, must be changed for each file system
+% adding the paths allows you to run different functions in all the
+% folders, without being in those folders.
+
+%CMJ macbook pro
+% main path folder for code
+addpath('/Users/cjakobson/Dropbox/Berkeley/Chrisoldlabstuff/pdumodeling/Pdu/matlab')
+% numerical code path
+addpath('/Users/cjakobson/Dropbox/Berkeley/Chrisoldlabstuff/pdumodeling/Pdu/matlab/numericalcode')
+% analytical code path
+addpath('/Users/cjakobson/Dropbox/Berkeley/Chrisoldlabstuff/pdumodeling/Pdu/matlab/analyticcode')
+% figure code path
+addpath('/Users/cjakobson/Dropbox/Berkeley/Chrisoldlabstuff/pdumodeling/Pdu/matlab/figurescripts')
+
---
+++ figureScripts/figure2.m
@@ -0,0 +1,13 @@
+%script to generate Figure 2
+%CMJ 20160804
+clear variables
+
+figure('units','normalized','outerposition',[0 0 1 1])
+
+M=100;
+
+subplot(1,2,1)
+doubling_time_ConstantMCP('kcA',1,M);
+
+subplot(1,2,2)
+doubling_time_ConstantMCP('Pout',1,M);
